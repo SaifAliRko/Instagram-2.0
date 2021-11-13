674381cb4d803650199036549da5e262859316bf
SaifAliRko <saif.ali96587@gmail.com>
2021-11-13 09:58:25 +0100

    completed sign in with google

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,19 +1,30 @@
-import { getProviders, signIn as SignIntoProvider } from "next-auth/react"
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
 export default function signIn({ providers }) {
-    
-    console.log("proivders",providers)
+  console.log("proivders", providers);
   return (
     <>
-      { Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className="flex flex-col items-center justify-center  py-2 -mt-36 px-14 min-h-screen text-center">
+        <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+        <p className="font-xs italic">A dummy clone app tried for learning</p>
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="rounded-lg p-3 bg-blue-500 text-white"
+                onClick={() => SignIntoProvider(provider.id,{callbackUrl:'/'})}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
-  )
+  );
 }
 
 // This is the recommended way for Next.js 9.3 or newer
@@ -21,7 +32,7 @@ export async function getServerSideProps() {
   const providers = await getProviders();
   return {
     props: { providers },
-  }
+  };
 }
 
 /*
